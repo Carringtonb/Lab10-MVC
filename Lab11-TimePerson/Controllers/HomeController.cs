6b773cbabd3c1cbf380d9de140da74a8e7481c22
carrington <carringtonbeard@live.com>
2020-03-31 09:31:58 -0700

    scaffold done

---
+++ Lab11-TimePerson/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab11_TimePerson.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Default Home Route for our home page
+        /// </summary>
+        /// <returns>Generated View</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
